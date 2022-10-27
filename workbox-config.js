c2a250a161f161a2a8369c9bab183baf9a288d2b
MundoOra <mundoorar@gmail.com>
2022-10-27 09:51:49 -0500

    Initial commit

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'C:/Users/mundo/Downloads/Recetario',
+	globPatterns: [
+		'**/*.{css,scss,jfif,html,png,js,json}'
+	],
+	swDest: 'C:/Users/mundo/Downloads/Recetario/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
